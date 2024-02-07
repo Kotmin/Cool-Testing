8171d687ad9399bba1881c5db4341d9eb9fc927b
Kotmin <70173732+Kotmin@users.noreply.github.com>
2024-02-08 00:40:41 +0100

    fix typo

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="cool-testing",
     version="0.2",
     packages=find_packages(),
-        long_description=open('README.md').read(),
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
 )
