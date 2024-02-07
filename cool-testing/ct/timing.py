9dc1587eec5c474ca74b0d8adfaa32f8ce1b3662
kotmin <kotmin@users.noreply.github.com>
2024-02-07 23:50:35 +0100

    add custom loger func to override

--- cool-testing/ct/timing.py
+++ cool-testing/ct/timing.py
@@ -2,23 +2,32 @@ import atexit
 from time import time, strftime, localtime
 from datetime import timedelta
 from functools import wraps
+from typing import Callable, Optional
 
-def secondsToStr(elapsed=None):
+# Configuration class to hold custom settings
+class Config:
+    custom_logger: Optional[Callable[[str], None]] = None
+    time_format: str = "%Y-%m-%d %H:%M:%S"
+
+def secondsToStr(elapsed: Optional[float] = None) -> str:
     if elapsed is None:
-        return strftime("%Y-%m-%d %H:%M:%S", localtime())
+        return strftime(Config.time_format, localtime())
     else:
         return str(timedelta(seconds=elapsed))
 
-def log(s, elapsed=None):
-    line = "="*40
-    print(line)
-    print(secondsToStr(), '-', s)
+def log(s: str, elapsed: Optional[str] = None) -> None:
+    line = "=" * 40
+    message = f"{line}\n{secondsToStr()} - {s}\n"
     if elapsed:
-        print("Elapsed time:", elapsed)
-    print(line)
-    print()
+        message += f"Elapsed time: {elapsed}\n"
+    message += line + "\n"
+    
+    if Config.custom_logger:
+        Config.custom_logger(message)
+    else:
+        print(message)
 
-def timing(func):
+def timing(func: Callable) -> Callable:
     @wraps(func)
     def wrapper(*args, **kwargs):
         start = time()
@@ -29,3 +38,9 @@ def timing(func):
         return result
     return wrapper
 
+# Functions to set custom logger and time format
+def set_custom_logger(logger_func: Callable[[str], None]) -> None:
+    Config.custom_logger = logger_func
+
+def set_time_format(format_str: str) -> None:
+    Config.time_format = format_str
